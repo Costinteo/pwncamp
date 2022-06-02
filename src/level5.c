805d2068a067b6d0a43adaf29d2eff74f6f883d0
Costinteo <39965799+Costinteo@users.noreply.github.com>
2022-06-02 19:56:15 +0300

    Finished level5

--- src/level5.c
+++ src/level5.c
@@ -6,33 +6,88 @@ const char PASSWORD[] = "\x40\x2c\x7a\x22\x26\x26\x73\x30\x56\x1b\x79\x2c\x39\x2
 FILE * VAULT;
 char VAULT_NAME[] = "\x04\x5a\x4b\x59\x59\x75\x5c\x4b\x5f\x46\x5e";
 
-void xcrypt(char * pass, const char * key) {
-	int pos = 0;
-	for (;*pass;*pass^key[pos++%strlen(key)], pass++);
+void xopen_vault(const char * mode) {
+	setreuid(geteuid(), geteuid());
+	memfrob(VAULT_NAME, strlen(VAULT_NAME));
+	VAULT = fopen(VAULT_NAME, mode);
+	memfrob(VAULT_NAME, strlen(VAULT_NAME));
 }
 
-void store_pass(const char * pass, const char * platform) {
+void xreset_vault() {
+	xopen_vault("w");
+	fclose(VAULT);
+}
+
+void xcrypt(char * pass, const char * key) {
+	int pos = 0;
+	for (;*pass;*pass=*pass+key[pos%strlen(key)], pass++, pos++);
+}
+
+void store_pass(const char * platform, char * pass, const char * key) {
+	xopen_vault("a+");
 	char entry[64] = "";
+	xcrypt(pass, key);
 	strncat(entry, platform, strlen(platform));
 	strcat(entry, ":");
 	strncat(entry, pass, strlen(pass));
+	fputs(entry, VAULT);
+	fputs("\n", VAULT);
+	fclose(VAULT);
+
 }
 
-void load_pass(const char * plaftorm) {
-
-}
-
-int main(int argc, char * argv[]) {
-	memfrob(VAULT_NAME, strlen(VAULT_NAME));
-	VAULT = fopen(VAULT_NAME, "a+");
+void load_pass(const char * key, const char * platform) {
+	xopen_vault("r");
 	char line[256];
-	fgets(line, sizeof(line), VAULT);
-	puts(line);
-	// get arguments
-	// decide what you're going to do
-	// password manager
-	// hidden buffer overflow (strncat?)
-	// decrypt password hidden in pass.txt
-	// use some stupid encryption, leaking info by xoring some parts twice by mistake
+	while (fgets(line, sizeof(line), VAULT)) {
+		char * delim = strchr(line, ':');
+		*delim = '\0';
+		if (!strcmp(line, platform)) {
+			char password[64] = "";
+			strcpy(password, delim + 1);
+			password[strlen(password)] = '\0';
+			xcrypt(password, key);
+			puts(password);
+		}
+	}
+	fclose(VAULT);
+}
+
+void print_menu() {
+	puts("Welcome to xCrypt password manager! What do you want to do?");
+	char menu[][256] = {
+		"Store password;",
+		"Load password;",
+		"Reset vault;",
+		"Exit;"
+	};
+	for (int i = 0; i < sizeof(menu) / sizeof(menu[0]); i++) {
+		printf("%d. %s\n", i + 1, menu[i]);
+	}
+}
+
+int main() {
+	int option;
+	char platform[128], pass[128], key[64];
+	print_menu();
+	scanf("%d", &option);
+
+	switch (option) {
+		case 1:
+			puts("Input platform, password and key, separated by whitespaces (PLATFORM PASSWORD KEY):");
+			scanf("%127s %127s %63s", platform, pass, key);
+			store_pass(platform, pass, key);
+			break;
+		case 2:
+			puts("Input platform and key, separated by whitespaces (PLATFORM KEY):");
+			scanf("%127s %63s", platform, key);
+			load_pass(key, platform);
+			break;
+		case 3:
+			xreset_vault();
+			break;
+		case 4:
+			break;
+	}
 	return 0;
 }
